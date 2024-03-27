9b5d7925b88b4bce9e96e0fa6805736557171f7b
hjx <1747054060@qq.com>
2024-03-27 23:18:34 +0800

    lsc

--- docs/ConvexOptimization/ConvexFunction/convexfunc.py
+++ docs/ConvexOptimization/ConvexFunction/convexfunc.py
@@ -16,24 +16,24 @@ class ConvexFunc(Scene):
             tips=False,
         )
         axes.add_coordinates()
-        self.play(Create(axes))
+        self.add(axes)
 
         def func(x):
             return 1/2 * x**2
 
         curve = axes.plot(func, x_range=[-3, 3], color=RED)
-        self.play(Create(curve))
+        self.add(curve)
 
         x1 = -1
         x2 = 1.2
-        self.play(
-            Create(Dot(axes.c2p(x1, func(x1)), color=YELLOW)),
-            Create(Dot(axes.c2p(x2, func(x2)), color=YELLOW))
+        self.add(
+            Dot(axes.c2p(x1, func(x1)), color=YELLOW),
+            Dot(axes.c2p(x2, func(x2)), color=YELLOW)
         )
         line = Line(axes.c2p(x1, func(x1)), axes.c2p(x2, func(x2)), color=YELLOW)
-        self.play( Create(line) )
+        self.add( line )
 
-        self.wait()
+        
         
 
 class Epi(Scene):
@@ -68,3 +68,49 @@ class Epi(Scene):
 
         self.add(Tex(r"$f(x)$", color=WHITE).shift(RIGHT*2+UP*-1))
         self.add(Tex("$epi(f)$").shift(UP*1))
+
+
+class lsc(Scene):
+    """
+    下半连续函数
+    """
+    def construct(self):
+        x_range = [-3, 3, 1]
+        y_range = [-3, 3, 1]
+        axes = Axes(
+            x_range=x_range,
+            y_range=y_range,
+            x_length=8,
+            y_length=8,
+            axis_config={"stroke_color": BLUE},
+            tips=False,
+        )
+        axes.add_coordinates()
+        self.add(axes)
+
+        x1 = -1
+        x2 = 1.2
+        def funcbase(x):
+            return 1/2 * x**2
+        
+        def func(x):
+            if x >= x1 and x <= x2:
+                return funcbase(x) - 1
+            else:
+                return funcbase(x)
+            
+        axes.plot(func, x_range=[-3, 3], color=RED)
+        b_curve = axes.plot(lambda x: 5, x_range=[-3, 3], color=RED)
+        curve = axes.plot(func, x_range=[-3, 3], color=RED, discontinuities=[x1, x2])
+        fill_region = axes.get_area(curve, x_range=[-3, 3], color=BLUE_E, opacity=0.5, bounded_graph=b_curve)
+        self.add(fill_region)
+        self.add(curve)
+
+        d1 = Circle(radius=.1, color=RED).shift(axes.c2p(x1, func(x1))).set_fill(color=RED, opacity=1)
+        d2 = Circle(radius=.1, color=RED).shift(axes.c2p(x2, func(x2))).set_fill(color=RED, opacity=1)
+
+        d3 = Circle(radius=.1, color=RED).set_fill(opacity=0).shift(axes.c2p(x1, funcbase(x1)))
+        d4 = Circle(radius=.1, color=RED).set_fill(opacity=0).shift(axes.c2p(x2, funcbase(x2)))
+
+        self.add(d1, d2, d3, d4)
+        
--- docs/ConvexOptimization/ConvexFunction/index.md
+++ docs/ConvexOptimization/ConvexFunction/index.md
@@ -1,12 +1,35 @@
 # 凸函数
 
-<video src="./media/videos/convexfunc/720p30/ConvexFunc.mp4" width="100%"  type="video/mp4" controls="controls" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></video>
+函数 \(f：R^n→R\) 是 **凸**(convex) 的，如果 \(\text{dom} f\) 是凸集，且对所有的 \(x，y∈\text{dom} f\)，和 \(0≤θ≤1\) ，都有
 
+$$ f(\theta x+(1-\theta y)) ≤ \theta f(x)+(1-\theta)f(y) $$
+<!-- 
+<video src="./media/videos/convexfunc/720p30/ConvexFunc.mp4" width="100%"  type="video/mp4" controls="controls" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></video> -->
+
+![convexfunc](media/images/convexfunc/ConvexFunc_ManimCE_v0.18.0.png)
 
 
 ## 上方图
 
+$$ epi(f) = \{ (x, t) \in R^{n+1} | f(x) ≤ t \} $$
+
 ![epi](media/images/convexfunc/Epi_ManimCE_v0.18.0.png)
 
+!!! theorem
+    \(f\) 是凸函数，当且仅当 \(epi(f)\) 是凸集。
 
-## 下半连续
+
+## 下半连续函数
+
+定义1：\(epi(f)\) 是闭集，则称 \(f\) 是下半连续函数。
+
+定义2：对任意的 \(x\in \text{dom} f\)，和序列 \(\{y_n\}, \lim_{n→∞} y_n = x\)，有
+
+$$ f(x) ≤ \liminf_{n→∞} f(y_n) $$
+
+则称 \(f\) 是下半连续函数。
+
+!!! note
+    这两种定义是等价的。
+
+![lsc](media/images/convexfunc/lsc_ManimCE_v0.18.0.png)
